c68fb10db56ae3ffa0aeb81fa3035da82b4ac261
Dan Garro <dobetterwithdan@gmail.com>
2023-03-18 15:24:54 -0400

    finished models for backend

--- backend_api/models.py
+++ backend_api/models.py
@@ -34,10 +34,64 @@ class Education(models.Model):
     school_state = models.CharField(max_length=200, blank=True)
     school_country = models.CharField(max_length=200)
     degree_earned = models.CharField(max_length=200, blank=True)
-    discipline = models.CharField(max_length=125, blank=True)
+    program = models.CharField(max_length=125, blank=True)
     date_started = models.DateField()
     date_completed = models.DateField()
     accolades = models.CharField(max_length=250, blank=True)
 
     def __str__(self):
         return f"{self.school_name.title()} | {self.date_completed}"
+
+
+class Experience(models.Model):
+    """a model representing work experience"""
+
+    position = models.CharField(max_length=200)
+    employer_name = models.CharField(max_length=250)
+    employer_city = models.CharField(max_length=200, blank=True)
+    employer_state = models.CharField(max_length=150, blank=True)
+    employer_country = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True)
+    current_employer = models.BooleanField(default=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.position} | {self.employer_name.title()} | {self.start_date}"
+
+
+class Project(models.Model):
+    """a model representing coding projects on resume"""
+
+    title = models.CharField(max_length=250)
+    tech_used = models.CharField(max_length=300)
+    description = models.TextField()
+    git_repo = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"{self.title.title()}"
+
+
+class Website(models.Model):
+    """a model representing domains"""
+
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+    description = models.CharField(max_length=175, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}: {self.url}"
+
+
+class Hobby(models.Model):
+    """a model representing hobbies or additional info from resume"""
+
+    name = models.CharField(max_length=200)
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True)
+    description = models.TextField(blank=True)
+    url = models.URLField(blank=True)
+    additional_info = models.CharField(max_length=250, blank=True)
+
+    def __str__(self):
+        return f"{self.name.title()}"
